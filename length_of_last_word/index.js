22b46c38f2e7b2e6634fec50a93ede3d9fd189df
armprogmath <armprogmath@gmail.com>
2025-11-14 18:38:55 +0400

    HiLoad-Book

--- length_of_last_word/index.js
+++ length_of_last_word/index.js
@@ -8,20 +8,20 @@
 
 //console.log(lengthOfLastWord("hello world12 "))
 
-// var lengthOfLastWord = function(s) {
-//     let i = s.length - 1;
-//     let length = 0;
-//
-//     // skip trailing spaces
-//     while (i >= 0 && s[i] === ' ') i--;
-//
-//     // count letters of last word
-//     while (i >= 0 && s[i] !== ' ') {
-//         length++;
-//         i--;
-//     }
-//
-//     return length;
-// };
-//
-// console.log(lengthOfLastWord("Hello world "))
+var lengthOfLastWord = function(s) {
+    let i = s.length - 1;
+    let length = 0;
+
+    // skip trailing spaces
+    while (i >= 0 && s[i] === ' ') i--;
+
+    // count letters of last word
+    while (i >= 0 && s[i] !== ' ') {
+        length++;
+        i--;
+    }
+
+    return length;
+};
+
+console.log(lengthOfLastWord("Hello world "))
