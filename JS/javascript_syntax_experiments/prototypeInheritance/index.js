33b48adbad983be8d1766fd483a7eda61b38c595
armprogmath <armprogmath@gmail.com>
2025-12-02 21:05:22 +0400

    last-word

--- JS/javascript_syntax_experiments/prototypeInheritance/index.js
+++ JS/javascript_syntax_experiments/prototypeInheritance/index.js
@@ -80,21 +80,21 @@
 /////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////
 
-// let animal = {
-//     jumps: null
-// };
-//
-// let rabbit = {
-//     __proto__: animal,
-//     jumps: true
-// };
-//
-// console.log( rabbit.jumps ); // true (1)
-//
-// delete rabbit.jumps;
-//
-// console.log( rabbit.jumps ); // null (2)
-//
-// delete animal.jumps;
-//
-// console.log( rabbit.jumps ); // undefined (3)
+let animal = {
+    jumps: null
+};
+
+let rabbit = {
+    __proto__: animal,
+    jumps: true
+};
+
+console.log( rabbit.jumps ); // true (1)
+
+delete rabbit.jumps;
+
+console.log( rabbit.jumps ); // null (2)
+
+delete animal.jumps;
+
+console.log( rabbit.jumps ); // undefined (3)
