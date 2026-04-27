e0bc526e4ccd19991869ac92861eaa87c34a1343
armprogmath <armprogmath@gmail.com>
2026-04-27 22:23:48 +0400

    fixed configuration file

--- memoFun/index.js
+++ memoFun/index.js
@@ -8,7 +8,7 @@ function sum(...args) {
 const memoizedSum = memo(sum);
 
 // Test with primitives
-//console.log(memoizedSum(1, 3, 2)); // Calculates
+console.log(memoizedSum(1, 3, 2)); // Calculates
 
 console.log(memoizedSum(1, 2, 3)); // From cache
 
